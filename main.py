91dc3dd43993d6712f37ed75046c258c623ee45b
jedi24garcia <jedi24garcia@gmail.com>
2024-03-22 11:00:56 +1300

    initial 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("Hello, Wellington")
+        print("Hello, Wellington!")
 
 if __name__ == '__main__':
         SayHello()
